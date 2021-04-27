f4ff9289b51fef1c550c9fbfba7f39b54a24e34f
FacundoPonce <mrfacu125@gmail.com>
2021-04-27 10:21:16 -0300

    Crossair set and fire impact placed when shoot.

--- ShooterGame-PonceFacundo/Assets/Scripts/Gun.cs
+++ ShooterGame-PonceFacundo/Assets/Scripts/Gun.cs
@@ -1,28 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
-    [SerializeField] public List<GameObject> bulletsShooted;
-    [SerializeField] private Transform gunPosition; 
-    [SerializeField] private GameObject prefabBullet;
+    [SerializeField] private ParticleSystem prefabBulletImpact;
+    [SerializeField] private Text ammo;
     [SerializeField] public Camera viewPlayer;
     [SerializeField] private float maxRange;
 
     public int clipSize;
     public int actualMagazine;
     public int maxAmmoToCarry;
-
+    private Ray myRayDestiny;
     private void Start()
     {
-        bulletsShooted.Clear();
         actualMagazine = clipSize;
     }
 
     private void Update()
     {
+        ShowAmmo();
+        
         Shoot();
+
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadGun();
+        }    
     }
 
     public void Shoot()
@@ -30,19 +36,27 @@ public class Gun : MonoBehaviour
         if (actualMagazine > 0)
         {
             Vector3 mousePos = Input.mousePosition;
-            Vector3 myRayOrigin = gunPosition.position;
-            Ray myRayDestiny = viewPlayer.ScreenPointToRay(mousePos);
-            Debug.DrawRay(myRayOrigin, myRayDestiny.direction * maxRange, Color.red);
+            myRayDestiny = viewPlayer.ScreenPointToRay(mousePos);
+            Debug.DrawRay(myRayDestiny.origin, myRayDestiny.direction * maxRange, Color.red);
 
             if (Input.GetButtonDown("Fire1"))
             {
                 RaycastHit myHit; 
-                if(Physics.Raycast(myRayOrigin, myRayDestiny.direction * maxRange, out myHit))
+                if(Physics.Raycast(myRayDestiny.origin, myRayDestiny.direction * maxRange, out myHit))
                 {
-                    bulletsShooted.Add(Instantiate(prefabBullet, gunPosition.position, Quaternion.identity, gunPosition));
+                    Instantiate(prefabBulletImpact, myHit.point, Quaternion.LookRotation(myHit.normal));
                     actualMagazine--;
                 }
             }
         }
+    }
+    public void ReloadGun()
+    {
+        actualMagazine = clipSize;
+    }
+
+    public void ShowAmmo()
+    {
+        ammo.text = actualMagazine.ToString() + "/" + clipSize.ToString();
     }
 }
