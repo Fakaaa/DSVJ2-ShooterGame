41a5df063ef76a259a991ac02549c58e38f93147
FacundoPonce <mrfacu125@gmail.com>
2021-05-03 15:26:29 -0300

    Update enemyFSM.cs.

--- ShooterGame-PonceFacundo/Assets/Scripts/EnemyFSM.cs
+++ ShooterGame-PonceFacundo/Assets/Scripts/EnemyFSM.cs
@@ -1,17 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyFSM : MonoBehaviour
 {
-    [SerializeField]
-    public enum STATES
-    {
-        Idle,
-        Attack,
-        GoBack
-    }
-    public STATES myState;
     [SerializeField]
     public enum TypeEnemy
     {
@@ -20,5 +10,57 @@ public class EnemyFSM : MonoBehaviour
     }
     public TypeEnemy myType;
 
+    [SerializeField]
+    public enum States
+    {
+        Idle,
+        Attack,
+        GoBack
+    }
+    public States myCurrentState;
 
+    [SerializeField][Tooltip("Only if type enemy is: BOMB")]
+    public float timeUntilExplode;
+    private float timer = 0;
+
+    [SerializeField]
+    [Range(5,20)]
+    [Tooltip("Only if type enemy is: GHOST")]
+    private float speedEnemy;
+
+    [SerializeField] private float damageEnemy;
+
+    public void CheckState()
+    {
+        switch (myCurrentState)
+        {
+            case States.Idle:
+                MakeBehaviour(States.Idle, myType);
+                break;
+            case States.Attack:
+                MakeBehaviour(States.Attack, myType);
+                break;
+            case States.GoBack:
+                MakeBehaviour(States.GoBack, myType);
+                break;
+        }
+    }
+    public void MakeBehaviour(States whatState, TypeEnemy whatType)
+    {
+        switch (whatType)
+        {
+            case TypeEnemy.Ghost:
+
+
+                break;
+            case TypeEnemy.Bomb:
+
+                if(whatState == States.Idle)
+                {
+                    transform.position = transform.position;
+                }
+
+                break;
+        }
+    }
 }
